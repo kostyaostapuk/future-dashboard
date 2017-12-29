0ed92a6c12e96c486147546dbbf601947c7b6f84
Kostya Ostapuk <ostapuk97@gmail.com>
2017-12-29 16:45:11 +0300

    themes cosmic and default

---
+++ src/app/@theme/theme.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import{ COSMIC_THEME} from './styles/theme.cosmic';
+import{ DEFAULT_THEME} from './styles/theme.default';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class themeModule { }
