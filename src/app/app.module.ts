2d0ebd003c9d297b231d25c311ed6d5f099d7ec2
kostyaostapuk <ostapuk97@gmail.com>
2018-01-09 16:23:43 +0300

    router fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { PagesModule } from './pages/pages.module';
 import { themeModule} from './@theme/theme.module';
+import { DashboardComponent} from './pages/dashboard/dashboard.component';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent} from './navbar/navbar.component';
 import { SidebarComponent} from './sidebar/sidebar.component';
-import { AppRoutingModule } from './app-routing.module';
-
+import { Routes, RouterModule } from '@angular/router';
+import { GoodsBoardComponent } from './goods-board/goods-board.component';
+// import { AppRoutingModule } from './app-routing.module';
+const routes: Routes = [
+  {path:'', component: DashboardComponent },
+	{path:'goods-board', component: GoodsBoardComponent }
+];
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavbarComponent,
 		SidebarComponent,
+		GoodsBoardComponent
 	],
 	imports: [
-		AppRoutingModule,
+		// AppRoutingModule,
+		RouterModule.forRoot(routes, {useHash:true}),
 		BrowserModule,
 		PagesModule,
 		themeModule
@@ -24,4 +32,5 @@ import { AppRoutingModule } from './app-routing.module';
 	providers: [],
 	bootstrap: [AppComponent]
 })
+
 export class AppModule { }
